c45a5d679241d0f35f0c141a39a995a3d222d437
Anubhav Singh <anubhavsssingh4@gmail.com>
2024-05-23 11:12:32 +0530

    ui fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Header = () => {
   return (
     <header className="bg-gray-700 body-font bg-header">
-    <div className="container mx-auto flex flex-wrap p-3 flex-col md:flex-row items-center justify-between">
+    <div className="container mx-auto flex  p-3 flex-row items-center justify-between">
      
-        <p className="ml-5 text-xl text-white font-outfit font-semibold">envato<span className='text-green-400 font-sora font-light'>market</span></p>
+        <p className="md:-ml-10 text-xl text-white font-outfit font-semibold">envato<span className='text-green-400 font-sora font-light'>market</span></p>
         <button className="bg-green-500 hover:bg-green-400 text-white font-bold py-1 px-4 border-b-4 border-green-700 hover:green-blue-500 rounded">
   Button
 </button>
